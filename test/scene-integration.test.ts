24c1aad0583601009d11d394df68155dd6aea1d7
jimbojw <wilson.jim.r@gmail.com>
2022-09-19 12:54:53 -0400

    Adding unit tests for hitTest() with exclusive checks.

--- test/scene-integration.test.ts
+++ test/scene-integration.test.ts
@@ -528,8 +528,8 @@ describe('Scene', () => {
     }
   });
 
-  describe('hitTest()', () => {
-    const section = createSection('Scene::hitTest');
+  describe('hitTest() inclusive', () => {
+    const section = createSection('Scene::hitTest() inclusive');
     const sectionContent = section.querySelector('.content')!;
 
     for (let devicePixelRatio = 1; devicePixelRatio <= 3;
@@ -612,6 +612,101 @@ describe('Scene', () => {
           {
             params: {x: 190, y: 190, width: 0, height: 0, inclusive: true},
             expected: [-1, -1, -1, 3],
+          },
+        ];
+
+        for (const {params, expected} of tests) {
+          const res = scene.hitTest({...params, sprites});
+          expect(res[0]).toBeCloseTo(expected[0], 0);
+          expect(res[1]).toBeCloseTo(expected[1], 0);
+          expect(res[2]).toBeCloseTo(expected[2], 0);
+          expect(res[3]).toBeCloseTo(expected[3], 0);
+        }
+
+        // Cleanup REGL resources.
+        scene[SceneInternalSymbol].regl.destroy();
+      });
+    }
+  });
+
+  describe('hitTest() exclusive', () => {
+    const section = createSection('Scene::hitTest() exclusive');
+    const sectionContent = section.querySelector('.content')!;
+
+    for (let devicePixelRatio = 1; devicePixelRatio <= 3;
+         devicePixelRatio += 0.5) {
+      it(`should hit when devicePixelRatio=${devicePixelRatio}`, () => {
+        const container = document.createElement('div');
+        container.style.width = '200px';
+        container.style.height = '200px';
+        sectionContent.appendChild(container);
+
+        const timingFunctionsShim = new TimingFunctionsShim();
+
+        const scene = new Scene({
+          container,
+          defaultTransitionTimeMs: 0,
+          desiredSpriteCapacity: 10,
+          devicePixelRatio,
+          timingFunctions: timingFunctionsShim,
+        });
+
+        // Create four overlapping sprites.
+        const sprites = [
+          [-.25, .25],   // Index 0 = Top left.
+          [.25, .25],    // Index 1 = Top right.
+          [-.25, -.25],  // Index 2 = Bottom left.
+          [.25, -.25],   // Index 3 = Bottom right.
+        ].map((position) => {
+          const sprite = scene.createSprite();
+          sprite.enter((s: SpriteView) => {
+            s.BorderColorOpacity = .4;
+            s.BorderRadiusPixel = 10;
+            s.FillColor = [255, 255, 255, .4];
+            s.PositionWorld = position;
+            s.Sides = 1;
+            s.SizeWorld = .4;
+          })
+          return sprite;
+        });
+        timingFunctionsShim.runAnimationFrameCallbacks(3);
+
+        // Grid of nine tests, like a tic-tac-toe board.
+        const tests = [
+          // Top row.
+          {
+            params: {x: 5, y: 5, width: 190, height: 90, inclusive: false},
+            expected: [0, 1, -1, -1],
+          },
+          // Middle row.
+          {
+            params: {x: 5, y: 55, width: 190, height: 90, inclusive: false},
+            expected: [-1, -1, -1, -1],
+          },
+          // Bottom row.
+          {
+            params: {x: 5, y: 105, width: 190, height: 90, inclusive: false},
+            expected: [-1, -1, 2, 3],
+          },
+          // Left column.
+          {
+            params: {x: 5, y: 5, width: 90, height: 190, inclusive: false},
+            expected: [0, -1, 2, -1],
+          },
+          // Center column.
+          {
+            params: {x: 55, y: 5, width: 90, height: 190, inclusive: false},
+            expected: [-1, -1, -1, -1],
+          },
+          // Right column.
+          {
+            params: {x: 105, y: 5, width: 90, height: 190, inclusive: false},
+            expected: [-1, 1, -1, 3],
+          },
+          // All.
+          {
+            params: {x: 5, y: 5, width: 190, height: 190, inclusive: false},
+            expected: [0, 1, 2, 3],
           },
         ];
 
