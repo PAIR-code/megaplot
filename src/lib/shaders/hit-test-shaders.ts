951b0ed0ccff7e7305bf2d98b4c1c32e6267d59a
jimbojw <wilson.jim.r@gmail.com>
2022-09-21 14:27:47 -0400

    Fixing hitTest() shader to incorporate border into bounding box.

--- src/lib/shaders/hit-test-shaders.ts
+++ src/lib/shaders/hit-test-shaders.ts
@@ -80,6 +80,22 @@ export function vertexShader(
   return glsl`
 precision lowp float;
 
+/**
+ * WebGL vertex shaders output coordinates in clip space, which is a 3D volume
+ * where each component is clipped to the range (-1,1). The distance from
+ * edge-to-edge is therefore 2.
+ */
+const float CLIP_SPACE_RANGE = 2.;
+
+/**
+ * Each sprite receives the same vertex coordinates, which describe a unit
+ * square centered at the origin. However, the distance calculations performed
+ * by the fragment shader use a distance of 1 to mean the dead center of a
+ * circle, which implies a diameter of 2. So to convert from sprite vertex
+ * coordinate space to edge distance space requires a dilation of 2.
+ */
+const float EDGE_DISTANCE_DILATION = 2.;
+
 uniform float ts;
 
 uniform float devicePixelRatio;
@@ -215,6 +231,47 @@ void main () {
     currentMinSizePixel
   );
 
+  // Compute border attributes in parallel.
+  vec3 borderProperties = computeCurrentValue(
+      vec3(
+        previousBorderRadiusPixel(),
+        previousBorderRadiusRelative(),
+        previousBorderPlacement()),
+      vec3(
+        previousBorderRadiusPixelDelta(),
+        previousBorderRadiusRelativeDelta(),
+        previousBorderPlacementDelta()),
+      vec3(
+        targetBorderRadiusPixel(),
+        targetBorderRadiusRelative(),
+        targetBorderPlacement())
+  );
+  float currentBorderRadiusPixel = borderProperties.x;
+  float currentBorderRadiusRelative = borderProperties.y;
+  float currentBorderPlacement = borderProperties.z;
+
+  // Project the computed size into pixels by using the viewMatrixScale. Note
+  // that this already includes the effect of the devicePixelRatio, and a 2x
+  // multiplier for clip-space, which goes from -1 to 1 in all dimensions.
+  vec2 projectedSizePixel = computedSize.xy * viewMatrixScale.xy;
+
+  // Compute the distance from the sprite edge to the extent of the border. Used
+  // to shift the corners before hit testing to make sure the bounding box
+  // includes borders outside of the shape.
+  float edgeDistance = currentBorderRadiusRelative + (
+      currentBorderRadiusPixel *
+      CLIP_SPACE_RANGE *
+      EDGE_DISTANCE_DILATION *
+      devicePixelRatio /
+      min(projectedSizePixel.x, projectedSizePixel.y)
+    );
+
+  // Shift coorner vertices outward to account for borders, which may expand
+  // the bounding box of the sprite. XY are the bottom left corner, ZW are for
+  // the top right.
+  vec4 cornerCoordinates = vec4(-.5, -.5, .5, .5) *
+    (1. + edgeDistance * currentBorderPlacement);
+
   // Compute the current position component attributes.
   vec2 currentPositionPixel = computeCurrentValue(
       previousPositionPixel(),
@@ -238,7 +295,7 @@ void main () {
       computedSize,
       currentPositionRelative,
       currentPositionPixel,
-      vec2(-.5, -.5),
+      cornerCoordinates.xy,
       viewMatrix
   ) * .5 / devicePixelRatio;
   vec2 topRight = computeViewVertexPosition(
@@ -246,7 +303,7 @@ void main () {
       computedSize,
       currentPositionRelative,
       currentPositionPixel,
-      vec2(.5, .5),
+      cornerCoordinates.zw,
       viewMatrix
   ) * .5 / devicePixelRatio;
   vec4 spriteBox = vec4(bottomLeft.xy, topRight.xy - bottomLeft.xy);
