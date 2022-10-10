2e39738c95c25fb6bc7856ced7354342d06d1152
Ryan Mullins <ryanmullins@google.com>
2022-10-10 10:37:53 -0400

    Fixes #73. Adds explicit typing for inherited methods to TextSelection interface

--- src/lib/text-selection-types.d.ts
+++ src/lib/text-selection-types.d.ts
@@ -18,7 +18,7 @@
  * @fileoverview Types relating to text selections.
  */
 
-import {Selection} from './selection-types';
+import {Selection, SelectionCallback} from './selection-types';
 
 export type AlignmentOption = 'left'|'center'|'right';
 
@@ -29,6 +29,56 @@ export type VerticalAlignmentOption = 'top'|'middle'|'bottom';
  * text. Used for showing labels.
  */
 export interface TextSelection<T> extends Selection<T> {
+  /**
+   * Sets the callback to be invoked every time a sprite is bound to its datum.
+   * The bindCallback is invoked before the specific lifecycle phase callback
+   * (init, enter, update and exit).
+   */
+  onBind: (bindCallback: SelectionCallback<T>) => TextSelection<T>;
+
+  /**
+   * Sets the callback to be invoked before a sprite first appears. This is
+   * called after the onBind callback, and any update to the transition time
+   * will be ignored.
+   */
+  onInit: (initCallback: SelectionCallback<T>) => TextSelection<T>;
+
+  /**
+   * Set the callback to be invoked after the sprite first appears. Changes to
+   * the arrival time will be honored. This allows for transitioning properties
+   * as the sprite pops into existence.
+   */
+  onEnter: (enterCallback: SelectionCallback<T>) => TextSelection<T>;
+
+  /**
+   * Set the callback to be invoked after a previously bound sprite is bound
+   * again. This will not run when a sprite first comes into existence, only
+   * when its bound a subsequent time.
+   */
+  onUpdate: (updateCallback: SelectionCallback<T>) => TextSelection<T>;
+
+  /**
+   * Set the callback to be invoked for sprites that are being unbound.
+   */
+  onExit: (exitCallback: SelectionCallback<T>) => TextSelection<T>;
+
+  /**
+   * Bind the provided data points to the selection. This will cause the enter,
+   * update and exit callbacks to be invoked asynchronously. If a key function
+   * is specified, then data points will be bound to Sprites according to the
+   * key string produced.
+   */
+  bind: (data: T[], keyFn?: (datum: T) => string) => TextSelection<T>;
+
+  /**
+   * Clear any previously bound data and Sprites. Previously bound Sprites will
+   * still have their callbacks invoked. This is equivalent to calling bind()
+   * with an empty array, except that it is guaranteed to drop expsting data and
+   * Sprites, whereas calling bind([]) may be interrupted by a later call to
+   * bind().
+   */
+  clear: () => TextSelection<T>;
+
   /**
    * Set the callback for determining the string for the given datum.
    */
